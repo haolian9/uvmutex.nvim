f4ca075912bd5386d29abd032de3032c4ff21a1c
haoliang <haoliang0.1.2@gmail.com>
2023-03-23 14:21:38 +0800

    ~

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,21 @@
 const std = @import("std");
 
 const uv = @cImport(@cInclude("uv.h"));
+const allocator = std.heap.c_allocator;
 
-export fn uv_mutex_size() usize {
+export fn hal_uv_mutex_size() usize {
     return @sizeOf(uv.uv_mutex_t);
 }
 
-export fn uv_mutex_new(addr: *[@sizeOf(uv.uv_mutex_t)]u8) [*c]uv.uv_mutex_t {
+export fn hal_uv_mutex_new(addr: *[@sizeOf(uv.uv_mutex_t)]u8) [*c]uv.uv_mutex_t {
     return @ptrCast(*uv.uv_mutex_t, @alignCast(@alignOf(uv.uv_mutex_t), addr));
+}
+
+export fn hal_uv_mutex_create(addr: *usize) void {
+    const mutex = allocator.create(uv.uv_mutex_t) catch |err| @panic(@errorName(err));
+    addr.* = @ptrToInt(mutex);
+}
+
+export fn hal_uv_mutex_destroy(addr: *usize) void {
+    allocator.destroy(@intToPtr(*uv.uv_mutex_t, addr.*));
 }
--- test.lua
+++ test.lua
@@ -7,23 +7,45 @@ ffi.cdef([[
   int uv_mutex_trylock(uv_mutex_t *handle);
   void uv_mutex_unlock(uv_mutex_t *handle);
 
-  uint64_t uv_mutex_size();
-  uv_mutex_t *uv_mutex_new(char *addr);
+  uint64_t hal_uv_mutex_size();
+  uv_mutex_t *hal_uv_mutex_new(char *addr);
+
+  void hal_uv_mutex_create(uintptr_t *addr);
+  void hal_uv_mutex_destroy(uintptr_t *addr);
 ]])
 
 _ = ffi.load("zig-out/lib/libmain.so", true)
 local C = ffi.C
 
-local function main()
-  local mutex_raw = ffi.new("char[?]", C.uv_mutex_size())
-  local mutex = C.uv_mutex_new(mutex_raw)
-  print("init", C.uv_mutex_init(mutex))
-  print("trylock#1", C.uv_mutex_trylock(mutex))
-  print("trylock#2", C.uv_mutex_trylock(mutex))
-  -- todo: coredump on unlocking an un-acquired lock
-  C.uv_mutex_unlock(mutex)
-  -- todo: coredump when lock has not been unlocked
-  C.uv_mutex_destroy(mutex)
+local function by_pre_alloc_whole_memory()
+  local mutex = {}
+  do
+    mutex.addr = ffi.new("char[?]", C.hal_uv_mutex_size())
+    mutex.ptr = C.hal_uv_mutex_new(mutex.addr)
+  end
+  print("init", C.uv_mutex_init(mutex.ptr))
+  print("trylock#1", C.uv_mutex_trylock(mutex.ptr))
+  print("trylock#2", C.uv_mutex_trylock(mutex.ptr))
+  C.uv_mutex_unlock(mutex.ptr)
+  C.uv_mutex_destroy(mutex.ptr)
 end
 
-main()
+local function by_pre_alloc_ptr()
+  local addr = ffi.new("uintptr_t[1]")
+  C.hal_uv_mutex_create(addr)
+  local ok, err = pcall(function()
+    local mutex = ffi.cast("uv_mutex_t *", addr)
+    print("init", C.uv_mutex_init(mutex))
+    print("trylock#1", C.uv_mutex_trylock(mutex))
+    print("trylock#2", C.uv_mutex_trylock(mutex))
+    C.uv_mutex_unlock(mutex)
+    C.uv_mutex_destroy(mutex)
+  end)
+  -- todo: this operation conflicts with uv_mutex_destroy somehow
+  --       since we allocate the uv_mutex_t on the libmain side, without calling this,
+  --       its memory is surely leaked.
+  -- C.hal_uv_mutex_destroy(addr)
+  if not ok then error(err) end
+end
+
+by_pre_alloc_whole_memory()
